a324e0d4c9905650956594681988c2d238618878
martinnord <martin.nordstrom99@gmail.com>
2019-07-19 21:59:38 +0200

    test

--- src/components/modal/ModalContent.js
+++ src/components/modal/ModalContent.js
@@ -11,11 +11,7 @@ export const ModalContent = ({ toggleOpen, role = "dialog", onKeyDown }) => {
     return axios({
       method: "post",
       url: "https://cwb-landing-page-api.herokuapp.com/email_received",
-      data: { email },
-      withCredentials: true,
-      headers: {
-        "Content-Type": "application/json"
-      }
+      data: { email }
     }).then(res => console.log("RES", res));
   };
 
